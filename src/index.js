4eba0a38608bbb24c369aad9e056a8fa8646ae83
Dustin Schnee <schnee72@gmail.com>
2017-09-11 17:10:08 -0700

    style

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './store';
 import App from './containers/App';
+import './index.css';
 
 const root = document.getElementById('root');
 
